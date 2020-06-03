b1ba165e055e5864cc0871c8fec4307ffd5814f4
buriti <buriti@acception.com.bt>
2020-06-02 22:01:54 -0300

    Added try, catch.

--- js/createPoint.js
+++ js/createPoint.js
@@ -16,18 +16,22 @@ async function populateUfs() {
 populateUfs();
 
 async function getCities(event) {
-  const citySelect = document.querySelector('select[name=city]');
-  const stateInput = document.querySelector('[name=state]');
+  try {
+    const citySelect = document.querySelector('select[name=city]');
+    const stateInput = document.querySelector('[name=state]');
 
-  const indexOfSelectedState = event.target.selectedIndex;
-  stateInput.value = event.target.options[indexOfSelectedState].text
-  const ufValue = event.target.value;
-  const urlCities = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/distritos`;
+    const indexOfSelectedState = event.target.selectedIndex;
+    stateInput.value = event.target.options[indexOfSelectedState].text
+    const ufValue = event.target.value;
+    const urlCities = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/distritos`;
 
-  let dataResponse = await fetch(urlCities);
-  let cities = await dataResponse.json();
-  for (city of cities) {
-    citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
-  };
-  citySelect.disabled = false;
+    let dataResponse = await fetch(urlCities);
+    let cities = await dataResponse.json();
+    cities.forEach(city => {
+      citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+    });
+    citySelect.disabled = false;
+  } catch (error) {
+    console.log(error);
+  }
 }
