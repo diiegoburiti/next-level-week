58113843267b8d12331cc885645451917df04325
buriti <buriti@acception.com.bt>
2020-06-02 21:40:50 -0300

    Changed fetch to async.

--- js/createPoint.js
+++ js/createPoint.js
@@ -4,33 +4,30 @@ const ufSelect = document.querySelector('select[name=uf]');
 
 ufSelect.addEventListener('change', getCities);
 
-function populateUfs(params) {
-  fetch(urlStates)
-    .then((res) => res.json())
-    .then(states => {
-      for (state of states) {
-        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
-      }
-    });
+async function populateUfs() {
+
+  let dataResponse = await fetch(urlStates)
+  let states = await dataResponse.json()
+  for (state of states) {
+    ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
+  }
 }
+
 populateUfs();
 
-function getCities(event) {
+async function getCities(event) {
   const citySelect = document.querySelector('select[name=city]');
   const stateInput = document.querySelector('[name=state]');
-  console.log(stateInput, citySelect);
-  
+
   const indexOfSelectedState = event.target.selectedIndex;
   stateInput.value = event.target.options[indexOfSelectedState].text
   const ufValue = event.target.value;
   const urlCities = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/distritos`;
 
-  fetch(urlCities)
-    .then((res) => res.json())
-    .then(cities => {
-      for (city of cities) {
-        citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
-      };
-      citySelect.disabled = false;
-    });
+  let dataResponse = await fetch(urlCities);
+  let cities = await dataResponse.json();
+  for (city of cities) {
+    citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+  };
+  citySelect.disabled = false;
 }
